ca35366aabd9da8c9b31280a30b23931ec5d0ef0
Marzulu <marzuluimsong095@gmail.com>
2026-06-29 13:01:59 +0530

    fix: commit issue on rebase

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 print('Hello World')
 def add(a, b):
-<<<<<<< HEAD
-    if not (a%2==0 or b%2==0):
-        sum = a + b
-        print(f"Sum of {a} and {b} :{sum}")
-    else:
-        print("This function only adds odd numbers")
-=======
-    if a%2==0 and b%2==0:
-        sum = a + b
-        print(f"Sum of {a} and {b} :{sum}")
-    else:
-        print("This function only adds even numbers")
->>>>>>> 4935219 (feat: added a condition to add only even numbers)
+    sum = a + b
+    print(f"Sum of {a} and {b} :{sum}")
+
 add(1, 5)
