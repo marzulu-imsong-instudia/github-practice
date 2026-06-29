e8b5797dcbeb8fad871277433ec62ebfa95a3aca
Marzulu <marzuluimsong095@gmail.com>
2026-06-29 13:08:14 +0530

    feat: Added operators.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+from operators import *
 print('Hello World')
-def add(a, b):
-    sum = a + b
-    print(f"Sum of {a} and {b} :{sum}")
 
 add(1, 5)
---
+++ operators.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    sum = a + b
+    print(f"Sum of {a} and {b} :{sum}")
+
+def sub(a, b):
+    sub = a - b
+    print(f"Subtration of {a} anb {b} :{sub}")
